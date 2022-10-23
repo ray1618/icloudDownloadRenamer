7b0a20f29191c25b3c0d11f66dce5c1930e94a81
Raymond Bas <raymond.bas@cito.nl>
2022-10-23 19:47:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# iCloud download photo name prefixer
+
+As I downloaded my icloud images to my local storage. I found out al the names make no sense. It's hard to sort them by date without some kind of photo library as all the images are named img_XXXX.jpg
+
+What this script will do is put the date that the image is taken as a prefix of the original filename and in the process do the same thing to the "livephoto" .mov
+
+For example an image **img_XXXXX.jpg** & **img_img_XXXXX.mov** will be renamed to **2020-10-01-23-55-55_img_XXXX.jpg** & **2020-10-01-23-55-55_img_XXXX.mov**
+
+Luckely the images have exif available and we can use that to rename it.
+
+This script wil rename **.jpg**, **.png**, **.heic** and the corresponding **.mov** files that's used for live photo's. It will use the Exif DateTimeOriginal timestamp to create a dateprefix in the original filename
+
+## How to use:
+
+- Put your downloaded images to the images folder in the root.
+- Install the npm dependencies `npm install`
+- Then run `npm start`
+- Done now the
+
+> Make a copy of your images before you run this command and check the result before deleting the originals.
---
+++ rename.js
@@ -0,0 +1,142 @@
+const Fs = require('fs');
+const Path = require('path');
+const colors = require('colors');
+const fullPath = Path.join(__dirname, './images');
+const os = require('os');
+const ExifReader = require('exifreader');
+
+var ii = 0;
+
+function start() {
+  // get all files in images folder
+  // and rename images
+  Fs.readdir(fullPath, (error, files) => {
+    if (error) console.log(error);
+
+    const imageFiles = files.filter(isImage);
+    renameImageFiles(imageFiles);
+
+    // after renaming images get new file list
+    // and rename mov files
+    Fs.readdir(fullPath, (error, files2) => {
+      if (error) console.log(error);
+
+      renameMovFiles(files2);
+    });
+  });
+}
+
+function isImage(file) {
+  return (
+    file.indexOf('.heic') !== -1 ||
+    file.indexOf('.HEIC') !== -1 ||
+    file.indexOf('.png') !== -1 ||
+    file.indexOf('.PNG') !== -1 ||
+    file.indexOf('.jpg') !== -1 ||
+    file.indexOf('.JPG') !== -1 ||
+    file.indexOf('.jpeg') !== -1 ||
+    file.indexOf('.JPEG') !== -1
+  );
+}
+
+function renameImageFiles(imageFiles) {
+  imageFiles.forEach((imageFile) => {
+    const newImageFileName = createNewName(imageFile);
+
+    if (newImageFileName) {
+      renenameFile(imageFile, newImageFileName);
+    }
+  });
+}
+
+function renameMovFiles(files) {
+  const movFiles = files.filter((file) => {
+    return file.indexOf('.mov') !== -1 || file.indexOf('.MOV') !== -1;
+  });
+
+  movFiles.forEach((movFile) => {
+    // find .heic / img files that belong to mov file
+    const imageFile = files.find((file) => {
+      return getCorrespondingImageFiles(file, movFile);
+    });
+
+    if (imageFile) {
+      // get image name, and rename .mov with it
+      const newMovFileName = imageFile
+        .replace('.jpg', '.mov')
+        .replace('.JPG', '.MOV')
+        .replace('.jpeg', '.mov')
+        .replace('.JPEG', '.MOV')
+        .replace('.png', '.mov')
+        .replace('.PNG', '.MOV')
+        .replace('.heic', '.mov')
+        .replace('.HEIC', '.MOV');
+
+      renenameFile(movFile, newMovFileName);
+    }
+  });
+}
+
+function getCorrespondingImageFiles(file, movFile) {
+  const fileNameBase = movFile.replace('.mov', '').replace('.MOV', '');
+  return (
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.heic') !== -1) ||
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.HEIC') !== -1) ||
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.jpg') !== -1) ||
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.JPG') !== -1) ||
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.jpeg') !== -1) ||
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.JPEG') !== -1) ||
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.png') !== -1) ||
+    (file.indexOf(fileNameBase) !== -1 && file.indexOf('.PNG') !== -1)
+  );
+}
+
+function renenameFile(oldName, newName) {
+  const oldPath = Path.join(fullPath, oldName);
+  const newPath = Path.join(fullPath, newName);
+  const regExp =
+    /[0-9]{4}-([0-9]{2}|[0-9])-([0-9]{2}|[0-9])-([0-9]{2}|[0-9])-([0-9]{2}|[0-9])/;
+
+  // don't rename if names are the same
+  if (oldName === newName) {
+    return;
+  }
+
+  // don't rename when name already contains prefix date
+  if (regExp.test(oldName)) {
+    console.log('waning '.yellow + oldPath + ' seems to be renamed already');
+    return;
+  }
+
+  Fs.renameSync(oldPath, newPath);
+  console.log(oldName + ' renamed'.green + ' -> ' + newName);
+}
+
+function createNewName(fileImg) {
+  const tags = getExifTags(fileImg);
+
+  if (tags && tags.DateTimeOriginal) {
+    const dateTimeOriginal = tags.DateTimeOriginal.description;
+    const regExp = new RegExp(':', 'g');
+    const dateTimeString = dateTimeOriginal
+      .replace(regExp, '-')
+      .replace(' ', '-');
+    const newImageFileName = dateTimeString + '_' + fileImg;
+    return newImageFileName;
+  } else {
+    return null;
+  }
+}
+
+function getExifTags(fileImg) {
+  const filePath = Path.join(fullPath, fileImg);
+
+  const data = Fs.readFileSync(filePath);
+  const tags = ExifReader.load(data);
+  if (tags) {
+    delete tags['MakerNote'];
+  }
+  return tags;
+}
+
+start();
